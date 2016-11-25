b0eee15c4b0c2592723b8c1da787496dbca5b2b1
Greg Rowe <growe22@ufl.edu>
2016-11-25 13:56:13 -0600

    add Tooltipster jQuery plugin

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+// FitText plugin, enlarge heading text with window resize
 $('#name').fitText();
+
+//Tooltipster plugin, displays message when hovered over the image
+$(document).ready(function () {
+    $('.tooltip').tooltipster();
+});
